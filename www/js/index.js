738d4ad563cc8546438fe016166e98aafbdf3a4c
Pixelistik <code@pixelistik.de>
2015-09-27 23:29:20 +0200

    Extend proof of concept: read data from server

--- www/js/index.js
+++ www/js/index.js
@@ -5,19 +5,38 @@ var app = {
 
 	onDeviceReady: function() {
 		document.getElementById("save-list-button").addEventListener("click", function () {
+			var request = new XMLHttpRequest();
+		request.open('GET', 'http://map.ffdus.de/data/nodes.json', true);
+
+		request.onload = function() {
+		  if (this.status >= 200 && this.status < 400) {
+			// Success!
+			var data = JSON.parse(this.response);
+
 			window.resolveLocalFileSystemURL(cordova.file.externalRootDirectory, function(dir) {
 				dir.getFile("log.txt", {create:true}, function(file) {
-					console.log("got the file", file);
 					file.createWriter(function(fileWriter) {
-						var blob = new Blob(["Freifunk"], {type:'text/plain'});
+						var blob = new Blob([data.timestamp], {type:'text/plain'});
 						fileWriter.write(blob);
 					}, fail);
 				});
 			});
 
-			function fail(e) {
-				console.log("Error: " + e);
-			}
+		  } else {
+			// We reached our target server, but it returned an error
+
+		  }
+		};
+
+		request.onerror = function() {
+		  // There was a connection error of some sort
+		};
+
+		request.send();
+
+		function fail(e) {
+			console.log("Error: " + e);
+		}
 
 		});
 	},
