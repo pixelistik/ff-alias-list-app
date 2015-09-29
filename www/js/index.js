a74c5dde9034362fe21c45d4d5f8b0aea650aa6a
Pixelistik <code@pixelistik.de>
2015-09-29 22:48:43 +0200

    Fix indentation

--- www/js/index.js
+++ www/js/index.js
@@ -6,39 +6,39 @@ var app = {
 	onDeviceReady: function() {
 		document.getElementById("save-list-button").addEventListener("click", function () {
 			var request = new XMLHttpRequest();
-		request.open('GET', 'http://map.ffdus.de/data/nodes.json', true);
+			request.open('GET', 'http://map.ffdus.de/data/nodes.json', true);
 
-		request.onload = function() {
-		  if (this.status >= 200 && this.status < 400) {
-			// Success!
-			var data = JSON.parse(this.response);
+			request.onload = function() {
+			  if (this.status >= 200 && this.status < 400) {
+				// Success!
+				var data = JSON.parse(this.response);
 
-			var text = nodeListTransform(data).join("\n");
+				var text = nodeListTransform(data).join("\n");
 
-			window.resolveLocalFileSystemURL(cordova.file.externalRootDirectory, function(dir) {
-				dir.getFile("WifiAnalyzer_Alias.txt", {create:true}, function(file) {
-					file.createWriter(function(fileWriter) {
-						var blob = new Blob([text], {type:'text/plain'});
-						fileWriter.write(blob);
-					}, fail);
+				window.resolveLocalFileSystemURL(cordova.file.externalRootDirectory, function(dir) {
+					dir.getFile("WifiAnalyzer_Alias.txt", {create:true}, function(file) {
+						file.createWriter(function(fileWriter) {
+							var blob = new Blob([text], {type:'text/plain'});
+							fileWriter.write(blob);
+						}, fail);
+					});
 				});
-			});
 
-		  } else {
-			// We reached our target server, but it returned an error
+			  } else {
+				// We reached our target server, but it returned an error
 
-		  }
-		};
+			  }
+			};
 
-		request.onerror = function() {
-		  // There was a connection error of some sort
-		};
+			request.onerror = function() {
+			  // There was a connection error of some sort
+			};
 
-		request.send();
+			request.send();
 
-		function fail(e) {
-			console.log("Error: " + e);
-		}
+			function fail(e) {
+				console.log("Error: " + e);
+			}
 
 		});
 	},
