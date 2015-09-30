96e4c5f36a9c0dc689563748af03ff5161ba1e1c
Pixelistik <code@pixelistik.de>
2015-09-30 23:19:06 +0200

    Display some progress status texts

--- www/js/index.js
+++ www/js/index.js
@@ -4,40 +4,53 @@ var app = {
 	},
 
 	onDeviceReady: function() {
+		var statusText = document.getElementById("status");
+
 		document.getElementById("save-list-button").addEventListener("click", function () {
+			statusText.textContent = "Lade...";
+
 			var request = new XMLHttpRequest();
 			request.open('GET', 'http://map.ffdus.de/data/nodes.json', true);
 
 			request.onload = function() {
 			  if (this.status >= 200 && this.status < 400) {
 				// Success!
+				statusText.textContent = "Erstelle Liste...";
+
 				var data = JSON.parse(this.response);
 
 				var text = nodeListTransform(data).join("\n");
+
+				statusText.textContent = "Speichere Liste...";
 
 				window.resolveLocalFileSystemURL(cordova.file.externalRootDirectory, function(dir) {
 					dir.getFile("WifiAnalyzer_Alias.txt", {create:true}, function(file) {
 						file.createWriter(function(fileWriter) {
 							var blob = new Blob([text], {type:'text/plain'});
 							fileWriter.write(blob);
+							statusText.textContent = "OK";
+							window.setTimeout(function () {
+								statusText.textContent = "";
+							}, 2000);
 						}, fail);
 					});
 				});
 
 			  } else {
 				// We reached our target server, but it returned an error
-
+				statusText.textContent = "Serverfehler!";
 			  }
 			};
 
 			request.onerror = function() {
-			  // There was a connection error of some sort
+				// There was a connection error of some sort
+				statusText.textContent = "Verbindungsfehler! Hast du Internet?";
 			};
 
 			request.send();
 
 			function fail(e) {
-				console.log("Error: " + e);
+				statusText.textContent = "Exception: " + e;
 			}
 
 		});
