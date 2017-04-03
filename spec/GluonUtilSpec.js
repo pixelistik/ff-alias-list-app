665923ffe2ba2e26c538f5354e6187fb87393e45
Pixelistik <code@pixelistik.de>
2017-04-04 00:59:06 +0200

    Write a JS version of Gluon MAC derivation
    The MAC address derivation is now based on an MD5 hash.
    
    https://github.com/freifunk-gluon/gluon/blob/6a0ca58fc3fd0a86292274a4a0fd47a2cf474cff/package/gluon-core/luasrc/usr/lib/lua/gluon/util.lua#L187

---
+++ spec/GluonUtilSpec.js
@@ -0,0 +1,27 @@
+"use strict";
+
+var assert = require("chai").assert;
+var GluonUtil = require("../www/js/GluonUtil.js");
+
+describe("GluonUtil", function () {
+    /*
+    Test cases derived from running the Lua implementation
+    from
+    https://github.com/freifunk-gluon/gluon/blob/6a0ca58fc3fd0a86292274a4a0fd47a2cf474cff/package/gluon-core/luasrc/usr/lib/lua/gluon/util.lua#L187
+    */
+    it("should derive a MAC for network 1", function () {
+        var primaryMac = "c4:6e:1f:87:5e:f0";
+
+        var result = GluonUtil.generateMac(primaryMac, 3);
+
+        assert.equal(result, "66:73:38:76:45:33")
+    });
+
+    it("should derive a MAC for network 2", function () {
+        var primaryMac = "c4:6e:1f:87:5e:f0";
+
+        var result = GluonUtil.generateMac(primaryMac, 4);
+
+        assert.equal(result, "66:73:38:76:45:34")
+    });
+});
---
+++ www/js/GluonUtil.js
@@ -0,0 +1,48 @@
+"use strict";
+
+var md5 = require("blueimp-md5");
+
+(function (window) {
+    var GluonUtil = {
+        /*
+        Reimplementation of the Gluon derivation in Lua
+        https://github.com/freifunk-gluon/gluon/blob/6a0ca58fc3fd0a86292274a4a0fd47a2cf474cff/package/gluon-core/luasrc/usr/lib/lua/gluon/util.lua#L187
+        */
+        generateMac: function (primaryMac, i) {
+            var hashed = md5(primaryMac).substr(0, 12);
+
+            var m = [];
+
+            m.push(hashed.substr( 0, 2));
+            m.push(hashed.substr( 2, 2));
+            m.push(hashed.substr( 4, 2));
+            m.push(hashed.substr( 6, 2));
+            m.push(hashed.substr( 8, 2));
+            m.push(hashed.substr(10, 2));
+
+            m[1-1] = parseInt(m[1-1], 16);
+
+            m[1-1] = m[1-1] | 0x02;
+            m[1-1] = m[1-1] & 0xFE;
+
+            m[1-1] = m[1-1].toString(16);
+
+
+            m[6-1] = parseInt(m[6-1], 16);
+
+            m[6-1] = m[6-1] & 0xF8;
+            m[6-1] = m[6-1] + i;
+
+            m[6-1] = m[6-1].toString(16);
+
+            return m.join(":");
+        }
+    };
+
+    // Export as module or global
+    if (typeof module !== "undefined" && module.exports) {
+        module.exports = GluonUtil;
+    } else {
+        window.GluonUtil = GluonUtil;
+    }
+})(this);
